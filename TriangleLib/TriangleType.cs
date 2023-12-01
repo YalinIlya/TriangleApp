2c536e4ab94811a188baaf4fc56888d3e9b0b43d
YalinIlya <yalinilya@hotmail.com>
2023-12-02 00:18:24 +0300

    123

--- TriangleLib/TriangleType.cs
+++ TriangleLib/TriangleType.cs
@@ -12,16 +12,11 @@ namespace TriangleLib
                 && sideC + sideB > sideA))
                 return TriangleTypes.Error;
 
-            if (sideA == sideB && sideA == sideC)
-                return TriangleTypes.Acute;
-            else
-            {
-                decimal[] sidesArr = { sideA, sideB, sideC };
-                Array.Sort(sidesArr);
-                var catSquareSum = sidesArr[0] * sidesArr[0] + sidesArr[1] * sidesArr[1];
-                var gipSquare = sidesArr[2] * sidesArr[2];
-                return (TriangleTypes)gipSquare.CompareTo(catSquareSum);
-            }
+            decimal[] sidesArr = { sideA, sideB, sideC };
+            Array.Sort(sidesArr);
+            var catSquareSum = sidesArr[0] * sidesArr[0] + sidesArr[1] * sidesArr[1];
+            var gipSquare = sidesArr[2] * sidesArr[2];
+            return (TriangleTypes)gipSquare.CompareTo(catSquareSum);
         }
     }
 }
